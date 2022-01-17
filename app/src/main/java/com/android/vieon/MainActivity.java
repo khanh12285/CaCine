d374e180e54452e55d466f9acfe120d99a989afc
quangkz <52958320+quangkz@users.noreply.github.com>
2022-01-18 01:11:07 +0700

    firt commit

---
+++ app/src/main/java/com/android/vieon/MainActivity.java
@@ -0,0 +1,74 @@
+package com.android.vieon;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.drm.DefaultDrmSessionManager;
+import com.google.android.exoplayer2.drm.ExoMediaDrm;
+import com.google.android.exoplayer2.drm.FrameworkMediaDrm;
+import com.google.android.exoplayer2.drm.MediaDrmCallback;
+import com.google.android.exoplayer2.drm.MediaDrmCallbackException;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.ui.StyledPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
+
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        StyledPlayerView playerView = findViewById(R.id.playerView);
+
+        String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36";
+        DefaultHttpDataSource.Factory httpDataSourceFactory = new DefaultHttpDataSource.Factory();
+        httpDataSourceFactory.setUserAgent(userAgent);
+
+        DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(this, null, httpDataSourceFactory);
+
+        //link hết hạn thì get link khác
+        String playUrl = "https://hni-fpt-001-livecdn-vthn2-vnd.vieon.vn/cf9b935cee9b35cc7b119fd9b2086dbe/1642527536165/wz/livetv_origin_drm/kplus_1_22_08_2021.smil/manifest.mpd";
+        MediaSource mediaSource = new DashMediaSource.Factory(dataSourceFactory)
+                .setDrmSessionManager(new DefaultDrmSessionManager.Builder().setUuidAndExoMediaDrmProvider(C.CLEARKEY_UUID, FrameworkMediaDrm.DEFAULT_PROVIDER).setMultiSession(false).build(new MediaDrmCallback() {
+                    @Override
+                    public byte[] executeProvisionRequest(UUID uuid, ExoMediaDrm.ProvisionRequest request) throws MediaDrmCallbackException {
+                        return new byte[0];
+                    }
+
+                    @Override
+                    public byte[] executeKeyRequest(UUID uuid, ExoMediaDrm.KeyRequest request) throws MediaDrmCallbackException {
+                        return Base64.decode("eyJrZXlzIjpbeyJrdHkiOiJvY3QiLCJrIjoiTVVoTWpOUldpN3RoRUJxOEQ1a2ZYdyIsImtpZCI6IkxCaUlBa3BJT2tpNWkwak1FXy16bkEifV0sInR5cGUiOiJ0ZW1wb3JhcnkifQ==", 0);
+                    }
+                }))
+                .createMediaSource(MediaItem.fromUri(playUrl));
+        DefaultRenderersFactory renderersFactory = new DefaultRenderersFactory(this);
+        renderersFactory.setExtensionRendererMode(DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER);
+        SimpleExoPlayer player = new SimpleExoPlayer.Builder(this, renderersFactory).build();
+        player.setPlayWhenReady(true);
+        playerView.setPlayer(player);
+        player.setMediaSource(mediaSource);
+        player.prepare();
+    }
+}
